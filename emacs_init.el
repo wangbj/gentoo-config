ede0d5770aa9f0e118340382d973ffe9358cf4c9
Baojun Wang <wangbj@gmail.com>
2016-01-22 12:10:09 -0800

    Update emacs_init.el

--- emacs_init.el
+++ emacs_init.el
@@ -2,14 +2,16 @@
 (setq inhibit-startup-message t) 
 (column-number-mode t)
 
-(add-to-list 'default-frame-alist '(font . "Source Code Pro-10" ))
-(set-face-attribute 'default t :font "Source Code Pro-10" )
+(if (eq system-type 'gnu/linux)
+  (add-to-list 'default-frame-alist '(font . "Source Code Pro-10" ))
+  (set-face-attribute 'default t :font "Source Code Pro-10" )
+)
 
 (cua-mode 1)
 
-;; recursively add ~/.emacs.d/lisp
-;(let ((default-directory "~/.emacs.d/lisp/"))
-;    (normal-top-level-add-subdirs-to-load-path))
+;; recursively add ~/.emacs.d/
+(let ((default-directory "~/.emacs.d/elpa/"))
+    (normal-top-level-add-subdirs-to-load-path))
 
 ;; disable backup files
 (setq make-backup-files nil)
@@ -32,7 +34,9 @@
     (("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/")))))
 
-;(require 'cl-lib)
+;;(require 'cl-lib)
+
+(require 'haskell-mode)
 
 (add-to-list 'package-archives
          '("melpa" . "https://stable.melpa.org/packages/"))
@@ -42,14 +46,23 @@
     (package-refresh-contents))
 
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(custom-set-variables '(haskell-process-type 'stack-ghci))
+(add-hook 'haskell-interactive-mode-hook 'structured-haskell-repl-mode)
+(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
 
 (let ((my-cabal-path (expand-file-name "~/.cabal/bin")))
   (setenv "PATH" (concat my-cabal-path ":" (getenv "PATH")))
   (add-to-list 'exec-path my-cabal-path))
 
-(add-to-list 'load-path "~/.cabal/share/x86_64-linux-ghc-7.10.3/ghc-mod-5.5.0.0")
+(if (eq system-type 'darwin)
+  (add-to-list 'load-path "~/Library/Haskell/share/ghc-7.10.3-x86_64/ghc-mod-5.5.0.0/elisp")
+)
+(if (eq system-type 'gnu/linux)
+  (add-to-list 'load-path "~/.cabal/share/x86_64-linux-ghc-7.10.3/ghc-mod-5.5.0.0")
+)
 
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
+
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+
+(custom-set-variables '(haskell-process-type 'stack-ghci))
